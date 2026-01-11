b90bd396774021772d8a652bea28cdadd9596889
SOHAMPAL23 <palsoham074@gmail.com>
2026-01-11 23:23:05 +0530

    bat run backend

--- run_backend.bat
+++ run_backend.bat
@@ -1,4 +1,9 @@
 @echo off
-cd /d "C:\Users\Soham\OneDrive\Desktop\HeartDisease\backend"
+echo Starting Heart Disease Prediction Backend...
+echo.
+echo Make sure you have activated your virtual environment.
+echo Backend API will be available at: http://localhost:8002
+echo.
+cd /d "%~dp0\backend"
 python main.py
 pause
